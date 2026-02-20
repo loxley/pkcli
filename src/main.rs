72568e3ef2484d56e9369ff21b7b124e16b7f5be
Johan Sandström <loxley@users.noreply.github.com>
2026-02-20 09:02:23 +0100

    refactor: migrate to clap derive API

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-mod cli;
 use anyhow::Result;
+use clap::Parser;
+use pkcli::cli::Cli;
 use pkcli::run;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let matches = cli::build_cli().get_matches();
-    run(matches).await?;
+    let cli = Cli::parse();
+    run(cli).await?;
     Ok(())
 }
